4ebac0259dabb9f34e06dd62caa790e01d115ff6
Michel Megens <dev@michelmegens.net>
2015-03-30 03:58:17 +0200

    cmake: splitted the main CMakeLists.txt
    The main CMake configuration file has been splitted up in 5 parts:
    
    	* the main file: CMakeLists.txt
    	* sub project CMakeList.txt's (client/ and server/)
    	* scripts/directories.cmake
    	* scripts/options.cmake
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ scripts/directories.cmake
@@ -0,0 +1,3 @@
+set(XFIRE_INCLUDE_DIR include)
+set(XFIRE_CLIENT_PATH client)
+set(XFIRE_SERVER_PATH server)
---
+++ scripts/options.cmake
@@ -0,0 +1,6 @@
+option (XFIRE_SERVER
+	"Set to to true if the XFire server should be build"
+	[false])
+option (XFIRE_CLIENT
+	"Set to true if the XFire client should be build"
+	[false])
