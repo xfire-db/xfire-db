8e8864e5f4108e2da197d834928ca9e7f5ebf450
Michel Megens <dev@michelmegens.net>
2015-05-02 09:29:05 +0000

    engine: hash: added key order to reduce collisions
    Added an index counter to the generation of the hash seed to ensure
    different hashes for keys with the same characters in a different order.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- server/engine/hash.c
+++ server/engine/hash.c
@@ -43,15 +43,16 @@ static void fnv_hash(const char *data, u64 seed, u64 *key)
 
 static void xfire_calc_seed(const char *data, u64 *seed)
 {
-	int len, val;
-	u64 tmp;
+	int idx, len;
+	u64 tmp, val = 1ULL;
 	unsigned char *_data = (unsigned char *)data;
 
 	len = strlen(data);
-	val = 0;
+	idx = 1;
 	fnv_hash(data, FNV_BASE_HASH, &tmp);
 
 	while(*_data) {
+		val ^= *_data + idx;
 		val += *_data;
 		_data++;
 	}
