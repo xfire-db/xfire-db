a5fdc0becea8bfaf92de2b618a5010cf10308163
Michel Megens <dev@michelmegens.net>
2015-10-03 23:51:46 +0200

    doc: rbtree: added documentation on red black tree's.
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- server/include/xfire/rbtree.h
+++ server/include/xfire/rbtree.h
@@ -16,6 +16,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup rbtree
+ * @{
+ */
+
 #ifndef __RB_NODE_H__
 #define __RB_NODE_H__
 
@@ -26,26 +31,38 @@
 #include <xfire/types.h>
 #include <xfire/flags.h>
 
+/**
+ * @brief Red black tree node type.
+ */
 typedef struct rb_node {
-	struct rb_node *parent,
-		      *left,
-		      *right,
-		      *next,
-		      *prev;
+	struct rb_node *parent, //!< Node parent.
+		      *left, //!< Node left.
+		      *right, //!< Node right.
+		      *next, //!< Duplicate linked list pointer.
+		      *prev; //!< Linked list previous pointer.
 
-	u64 key;
-	atomic_flags_t flags;
+	u64 key; //!< List key.
+	atomic_flags_t flags; //!< Node flags.
 
-	atomic_t ldepth;
-	xfire_mutex_t lock;
-	xfire_cond_t condi;
+	atomic_t ldepth; //!< Depth of the node.
+	xfire_mutex_t lock; //!< Node lock.
+	xfire_cond_t condi; //!< Node condition.
 } RB_NODE;
 
+/**
+ * @brief Red black tree root.
+ */
 typedef struct rb_root {
-	struct rb_node *tree;
-	atomic64_t num;
-	xfire_spinlock_t lock;
+	struct rb_node *tree; //!< Root node.
+	atomic64_t num; //!< Number of nodes in the tree.
+	xfire_spinlock_t lock; //!< Root lock.
 
+	/**
+	 * @brief Duplicate compare function pointer.
+	 * @param node Node which has to be compared.
+	 * @param arg Configurable argument.
+	 * @return True if the node matches, false otherwise.
+	 */
 	bool (*cmp)(struct rb_node *node,const void*);
 
 } RB_ROOT;
@@ -83,6 +100,11 @@ extern struct rb_node *rb_get_node(struct rb_root *root, u64 key,
 					const void *arg);
 extern struct rb_node *__rb_get_node(struct rb_node *node);
 
+/**
+ * @brief Set the key of a node.
+ * @param node Node to set the key for.
+ * @param key Key to set.
+ */
 static inline void rb_set_key(struct rb_node *tree, u64 key)
 {
 	if(!tree)
@@ -91,25 +113,52 @@ static inline void rb_set_key(struct rb_node *tree, u64 key)
 	tree->key = key;
 }
 
+/**
+ * @brief Get the size of a red black tree.
+ * @param root Root to get the size of.
+ * @return The size of \p root.
+ */
 static inline s64 rb_get_size(struct rb_root *root)
 {
 	return atomic64_get(&root->num);
 }
 
+/**
+ * @brief Find out if a node has duplicates.
+ * @param node Node to check for duplicates.
+ * @return TRUE if \p node has duplicates, FALSE otherwise.
+ */
 static inline bool rb_node_has_duplicates(struct rb_node *node)
 {
 	return node->next != NULL;
 }
 
+/**
+ * @brief Get the root node from a tree.
+ * @param root Red black tree root to get the root node from.
+ * @return The root node.
+ */
 static inline struct rb_node *rb_get_root(struct rb_root *root)
 {
 	return root->tree;
 }
+
+/**
+ * @brief Set the root noe of a tree.
+ * @param root Red black tree to set the root node for.
+ * @param n Node to set as root.
+ * @note The current root node will be overridden.
+ */
 static inline void rb_set_root(struct rb_root *root, struct rb_node *n)
 {
 	root->tree = n;
 }
 
+/**
+ * @brief Check if a node is in a tree.
+ * @param node Node to check.
+ * @return TRUE if the node is not in a redblack tree, FALSE otherwise.
+ */
 static inline bool rb_unlinked(struct rb_node *node)
 {
 	if(!node)
@@ -121,67 +170,10 @@ static inline bool rb_unlinked(struct rb_node *node)
 	return false;
 }
 
-static inline void rb_swap_color(struct rb_node *n1, struct rb_node *n2)
-{
-	swap_bit(RB_NODE_RED_FLAG, &n1->flags, &n2->flags);
-	swap_bit(RB_NODE_DBLK_FLAG, &n1->flags, &n2->flags);
-}
-
-static inline bool rb_red(struct rb_node *n)
-{
-	if(!n)
-		return false;
-
-	return test_bit(RB_NODE_RED_FLAG, &n->flags);
-}
-
-static inline bool rb_blk(struct rb_node *n)
-{
-	if(!n)
-		return true;
-
-	if(test_bit(RB_NODE_RED_FLAG, &n->flags) ||
-			test_bit(RB_NODE_DBLK_FLAG, &n->flags))
-		return false;
-	
-	return true;
-}
-
-static inline void rb_set_dblk(struct rb_node *n)
-{
-	if(!n)
-		return;
-
-	clear_bit(RB_NODE_RED_FLAG, &n->flags);
-	set_bit(RB_NODE_DBLK_FLAG, &n->flags);
-}
-
-static inline void rb_set_red(struct rb_node *n)
-{
-	if(!n)
-		return;
-
-	clear_bit(RB_NODE_DBLK_FLAG, &n->flags);
-	set_bit(RB_NODE_RED_FLAG, &n->flags);
-}
-
-static inline void rb_set_blk(struct rb_node *n)
-{
-	if(!n)
-		return;
-
-	clear_bit(RB_NODE_DBLK_FLAG, &n->flags);
-	clear_bit(RB_NODE_RED_FLAG, &n->flags);
-}
-
-static inline bool rb_dblk(struct rb_node *n)
-{
-	if(!n)
-		return false;
-
-	return test_bit(RB_NODE_DBLK_FLAG, &n->flags);
-}
 
 CDECL_END
 
 #endif
+
+/** @} */
+
