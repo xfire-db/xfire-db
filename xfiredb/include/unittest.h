b530b276f6839ed3ef6958d0420d569adceafd4d
Michel Megens <dev@michelmegens.net>
2015-10-23 22:23:52 +0200

    test: core: added a unit testing module
    Added a unit testing module to automate test and increase the
    ease of doing them.
    
    All dictionary related tests are now automated.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ xfiredb/include/unittest.h
@@ -0,0 +1,38 @@
+/*
+ *  XFireDB unit testing frame work
+ *  Copyright (C) 2015   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __UNIT_TEST_H__
+#define __UNIT_TEST_H__
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include <xfire/xfire.h>
+#include <xfire/types.h>
+
+typedef void (*test_func_t)(void);
+
+#define TEST_SETUP(__x) \
+	extern test_func_t su; \
+	su == __x;
+#define TEST_TEARDOWN(__x) \
+	extern test_func_t td; \
+	td == __x;
+
+#endif
+
--- xfiredb/tests/dict/dict-test.cmake
+++ xfiredb/tests/dict/dict-test.cmake
@@ -1,13 +1,17 @@
 add_executable (dict-single
+		tests/unit_test.c
 		tests/dict/dict-single.c)
 
 add_executable (dict-iterator
+		tests/unit_test.c
 		tests/dict/dict-iterator.c)
 
 add_executable (dict-concurrent
+		tests/unit_test.c
 		tests/dict/dict-concurrent.c)
 
 add_executable (dict-database
+		tests/unit_test.c
 		tests/dict/dict-database.c)
 
 target_link_libraries (dict-single LINK_PUBLIC xfiredb)
