9ff05d45c06374e9cb212423177f9290f120294d
Michel Megens <dev@michelmegens.net>
2015-10-29 05:48:15 +0100

    test: removed the db-fill test
    This test has been replaced by the scripts/db_fill.rb script.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- libxfiredbengine/tests/core/core.cmake
+++ libxfiredbengine/tests/core/core.cmake
@@ -1,15 +1,12 @@
 add_executable (xfiredb-test
 		tests/unit_test.c
 		tests/core/xfiredb.c)
-add_executable (db-fill
-		tests/core/fill-db.c)
 add_executable (db-load
 		tests/core/db-load.c)
 
 add_executable (bitops-single
 		tests/core/bitops.c)
 
-target_link_libraries (db-fill LINK_PUBLIC xfiredbengine)
 target_link_libraries (db-load LINK_PUBLIC xfiredbengine)
 target_link_libraries (xfiredb-test LINK_PUBLIC xfiredbengine)
 target_link_libraries (bitops-single LINK_PUBLIC xfiredbengine)
