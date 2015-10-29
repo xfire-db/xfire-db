6f1cf4e3aa34ee5606902c01507b59b69ad19c2b
Michel Megens <dev@michelmegens.net>
2015-10-29 22:49:54 +0100

    cmake: added header file checks
    Added (verbose) header file checking at configuration time. Also the
    searching for library's is more verbose than before.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ scripts/find_deps.cmake
@@ -0,0 +1,47 @@
+MESSAGE( STATUS "looking for libsqlite3")
+find_library(SQLITE3_LIB
+	NAMES sqlite3
+)
+
+MESSAGE( STATUS "looking for libc")
+find_library(C_LIB
+	NAMES c
+)
+
+CHECK_INCLUDE_FILES(sqlite3.h SQLITE3_HEADER)
+CHECK_INCLUDE_FILES(stdlib.h STDLIB_HEADER)
+CHECK_INCLUDE_FILES(stdint.h STDINT_HEADER)
+CHECK_INCLUDE_FILES(stdio.h STDIO_HEADER)
+CHECK_INCLUDE_FILES(stdarg.h STDARG_HEADER)
+CHECK_INCLUDE_FILES(string.h STRING_HEADER)
+CHECK_INCLUDE_FILES(assert.h ASSERT_HEADER)
+CHECK_INCLUDE_FILES(ruby.h RUBY_HEADER)
+
+IF(NOT STRING_HEADER)
+	message( FATAL_ERROR "ruby.h is not found" )
+ENDIF()
+
+IF(NOT STRING_HEADER)
+	message( FATAL_ERROR "string.h is not found" )
+ENDIF()
+
+IF(NOT ASSERT_HEADER)
+	message( FATAL_ERROR "assert.h is not found" )
+ENDIF()
+
+IF(NOT STDINT_HEADER)
+	message( FATAL_ERROR "stdint.h is not found" )
+ENDIF()
+
+IF(NOT STDLIB_HEADER)
+	message( FATAL_ERROR "stdlib.h is not found" )
+ENDIF()
+
+IF(NOT STDIO_HEADER)
+	message( FATAL_ERROR "stdio.h is not found" )
+ENDIF()
+
+IF(NOT SQLITE3_HEADER)
+	message( FATAL_ERROR "sqlite3.h is not found" )
+ENDIF()
+
