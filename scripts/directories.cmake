8cd7bbc015c9dceb30ad0e345496e4bd30cb88f0
Michel Megens <dev@michelmegens.net>
2015-11-10 22:40:51 +0100

    project: renamed the ruby library directory
    Renamed the library directory from lib/xfiredb to lib/xfiredb-serv.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- scripts/directories.cmake
+++ scripts/directories.cmake
@@ -1,5 +1,5 @@
 set(XFIRE_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libxfiredbengine/include")
 set(XFIRE_CLIENT_PATH clients)
-set(XFIRE_EXTENSION_PATH ext/xfiredb)
+set(XFIRE_EXTENSION_PATH ext/xfiredb-serv)
 set(XFIRE_ENG_PATH libxfiredbengine)
 
