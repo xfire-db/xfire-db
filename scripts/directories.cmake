f35a1916b91051f123b4941a547a954c1931b350
Michel Megens <dev@michelmegens.net>
2015-11-22 19:04:28 +0100

    client: added a C client files
    Added a setup for a C client library.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- scripts/directories.cmake
+++ scripts/directories.cmake
@@ -1,5 +1,6 @@
-set(XFIRE_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libxfiredbengine/include")
+set(XFIRE_SERV_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libxfiredbengine/include")
 set(XFIRE_CLIENT_PATH clients)
+set(XFIRE_CLIENT_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/clients/c/include")
 set(XFIRE_EXTENSION_PATH ext/xfiredb-serv)
 set(XFIRE_ENG_PATH libxfiredbengine)
 
