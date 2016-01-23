9a4af2ea68871023cb4ef8479746098e22ce92b5
Michel Megens <dev@michelmegens.net>
2016-01-23 08:52:12 +0100

    client: doc: documented the xfiredb client header.
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- clients/c/include/xfiredb/xfiredb.h
+++ clients/c/include/xfiredb/xfiredb.h
@@ -34,81 +34,131 @@
 #define CDECL_END
 #endif
 
+/**
+ * @brief SSL client data structure.
+ */
 struct xfiredb_ssl_client {
-	SSL_CTX *ctx;
-	SSL *ssl;
+	SSL_CTX *ctx; //!< SSL CTX.
+	SSL *ssl; //!< SSL handle.
 };
 
+/**
+ * @brief XFireDB client data structure.
+ */
 struct xfiredb_client {
-	int socket;
-	struct sockaddr_in *serv;
-	struct xfiredb_ssl_client *ssl;
+	int socket; //!< TCP socket.
+	struct sockaddr_in *serv; //!< Socket address.
+	struct xfiredb_ssl_client *ssl; //!< SSL client handle.
 
-	long flags;
+	long flags; //!< Connection flags.
 };
 
+/**
+ * @brief Query results
+ */
 typedef enum {
-	XFIREDB_STRING,
-	XFIREDB_FIXNUM,
-	XFIREDB_STATUS,
-	XFIREDB_BOOL,
+	XFIREDB_STRING, //!< String result.
+	XFIREDB_FIXNUM, //!< Numeral result.
+	XFIREDB_STATUS, //!< Status code.
+	XFIREDB_BOOL,   //!< Boolean result.
 } xfiredb_result_type_t;
 
+/**
+ * @brief XFireDB query result data structure.
+ */
 struct xfiredb_result {
+	/**
+	 * @brief Result data.
+	 */
 	union {
-		char *ptr;
-		size_t ui;
-		ssize_t si;
-		unsigned char boolean;
+		char *ptr; //!< Pointer data.
+		size_t ui; //!< Unsigned numeral.
+		ssize_t si; //!< Signed numeral.
+		unsigned char boolean; //!< Boolean data.
 	} data;
 
-	int length;
+	int length; //!< Result length (size).
 
-	xfiredb_result_type_t type;
-	long status;
+	xfiredb_result_type_t type; //!< Result type.
+	long status; //!< Query status.
 };
 
-#define XFIREDB_RESULT_NIL 	0x1
-#define XFIREDB_RESULT_OK	0x2
-#define XFIREDB_RESULT_MSG	0x4
-#define XFIREDB_RESULT_SUCCESS	0x8
+#define XFIREDB_RESULT_NIL 	0x1 //!< NIL result
+#define XFIREDB_RESULT_OK	0x2 //!< Result code OK.
+#define XFIREDB_RESULT_MSG	0x4 //!< Indicates that string data is returned.
+#define XFIREDB_RESULT_SUCCESS	0x8 //!< Indicates the query was succesfull.
 
-#define XFIREDB_SOCK_STREAM	0x1
-#define XFIREDB_SOCK_RESOLV	0x2
-#define XFIREDB_SSL		0x4
-#define XFIREDB_AUTH		0x8
+#define XFIREDB_SOCK_STREAM	0x1 //!< Do not terminate the socket after the first query.
+#define XFIREDB_SOCK_RESOLV	0x2 //!< Hostname needs resolving.
+#define XFIREDB_SSL		0x4 //!< Connect using SSL.
+#define XFIREDB_AUTH		0x8 //!< Authenticate to the server using a username and password.
 
 CDECL
+/**
+ * @brief Get the result type.
+ * @param r Result to get the type from.
+ * @return The result type of \p r.
+ */
 static inline xfiredb_result_type_t xfiredb_result_type(struct xfiredb_result *r)
 {
 	return r->type;
 }
 
+/**
+ * @brief Get the result status.
+ * @param r Result to get the status of.
+ * @return The status of \p r.
+ */
 static inline long xfiredb_result_status(struct xfiredb_result *r)
 {
 	return r->status & (XFIREDB_RESULT_NIL | XFIREDB_RESULT_OK | XFIREDB_RESULT_MSG);
 }
 
+/**
+ * @brief Convert the value of \p r to a signed integer.
+ * @param r Result to get the integer data from.
+ * @return Integer data stored in \p r.
+ */
 static inline ssize_t xfiredb_result_to_int(struct xfiredb_result *r)
 {
 	return r->data.si;
 }
 
+/**
+ * @brief Get the unsigned integer value of \p r.
+ * @param r Result to get the uint value of.
+ * @return The integer data stored in \p r.
+ */
 static inline size_t xfiredb_result_to_uint(struct xfiredb_result *r)
 {
 	return r->data.ui;
 }
 
+/**
+ * @brief Get the boolean value of \p r.
+ * @param r Result to get the boolean value of.
+ * @return The boolean data stored in \p r.
+ */
 static inline unsigned char xfiredb_result_to_bool(struct xfiredb_result *r)
 {
 	return r->data.boolean;
 }
 
+/**
+ * @brief Get the data pointer stored in \p r.
+ * @param r Result to get the data pointer from.
+ * @return The data pointer stored in \p r.
+ */
 static inline char *xfiredb_result_to_ptr(struct xfiredb_result *r)
 {
 	return r->data.ptr;
 }
 
+/**
+ * @brief Check if a query was executed succesfully.
+ * @param r Result to check.
+ * @return Non-zero if the query was succesful, zero if the query failed.
+ */
 static inline int xfiredb_result_success(struct xfiredb_result *r)
 {
 	return !!(r->status & XFIREDB_RESULT_SUCCESS);
