d56037b552dca4e9083bcd5c90d02d368e1e0d3a
Michel Megens <dev@michelmegens.net>
2016-01-28 17:20:44 +0100

    xfiredb-cli: added getopts options to the XFireDB cli
    The command line interface now understands the options it needs to.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- clients/cli/cli.c
+++ clients/cli/cli.c
@@ -18,10 +18,67 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <wordexp.h>
 
 #include <xfiredb/xfiredb.h>
 
+#ifndef __cplusplus
+#define false 0
+#define true !false
+#endif
+
+static struct option long_opts[] = {
+	{"user",  required_argument, 0, 'u'},
+	{"pass",  required_argument, 0, 'p'},
+	{"ssl",    no_argument, 0, 's'},
+	{"help",    no_argument, 0, 'h'},
+};
+
+static void cli_usage(const char *prog)
+{
+	printf("Usage: %s [OPTS]\n", prog);
+}
+
+static void cli_help(const char *prog)
+{
+	cli_usage(prog);
+	printf("Connect to an XFireDB server using a command line interface. \n" \
+		"\n" \
+		"   -u, --user <username>       Username to use during authentication.\n" \
+		"   -p, --pass <passowd>        Password to use during authentication.\n" \
+		"   -s, --ssl                   Connect using SSL.\n" \
+		"   -h, --help                  Display this help text.\n");
+}
+
 int main(int argc, char **argv)
 {
+	int option, opt_idx = 0;
+
+	while(true) {
+		option = getopt_long(argc, argv, "u:p:sh", long_opts, &opt_idx);
+		if(option == -1)
+			break;
+
+		switch(option) {
+		case 'p':
+			break;
+		case 'u':
+			break;
+		case 's':
+			break;
+		case 'h':
+			cli_help(argv[0]);
+			exit(EXIT_SUCCESS);
+			break;
+		default:
+			cli_usage(argv[0]);
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
+
 	return -EXIT_SUCCESS;
 }
+
