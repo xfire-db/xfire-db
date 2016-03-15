cd49d42ae57fa122db2a18e619841a745f8a3408
Michel Megens <dev@michelmegens.net>
2016-03-15 03:18:26 +0100

    cmake: options: updated client build option name
    Renamed the XFIREDB_CLIENT option to XFIREDB_CLIENTS, there are multiple
    clients, after all.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- scripts/directories.cmake
+++ scripts/directories.cmake
@@ -1,6 +1,6 @@
 set(XFIREDB_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libxfiredbengine/include")
 set(XFIREDB_SQLITE_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/deps/sqlite3/include")
-set(XFIREDB_CLIENT_PATH clients)
+set(XFIREDB_CLIENTS_PATH clients)
 set(XFIREDB_EXTENSION_PATH ext/xfiredb-serv)
 set(XFIREDB_ENG_PATH libxfiredbengine)
 set(XFIREDB_CLIENT_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/clients/c/include")
